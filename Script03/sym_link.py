81ba2be46ae5f8768cc8ef762237fa691be20cb6
Muaz Osman <90202813+MuazOsmann@users.noreply.github.com>
2022-11-08 12:33:37 +0400

    Create sym_link.py

---
+++ Script03/sym_link.py
@@ -0,0 +1,19 @@
+import os
+
+
+def main():
+    LoopTerminator = False
+    while(LoopTerminator != True):
+        while True:
+            try:
+                print("Welcome to the Symbloic Link Script\n")
+                print("[1] Create a Symbloic Link\n")
+                print("[2] Delete a Link\n")
+                print("[3] Symbolic Links Summarized Report\n")
+                print("[quit] To Quit the Program\n")
+                #Taking the input here
+                input
+                #Decision taking mechanism to call the functions upon selection
+            except:
+                #Here is the exceptions to be added that we might face in the program running
+                pass
